216b58795ee27797d3cd72953f3f665ea607a8bb
unknown <edina.kurtic90@hotmail.com>
2021-03-31 10:32:35 +0200

    added ex05 Day23

---
+++ ex05/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import SimpleForm from './SimpleForm';
+
+
+function App() {
+  return (
+    <div className="App">
+     <SimpleForm/> 
+    </div>
+  );
+}
+
+export default App;
