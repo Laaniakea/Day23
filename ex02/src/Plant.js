f7941e52cd6c4337599f060c8c3c0d71d6662803
unknown <edina.kurtic90@hotmail.com>
2021-03-31 10:30:30 +0200

    added ex02 Day23

---
+++ ex02/src/Plant.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+class Plant extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { type: "Flowering plant" };
+    }
+    render() {
+        return (
+            <h2>Plant is {this.state.type}</h2>
+        )
+    }
+}
+
+export default Plant;
